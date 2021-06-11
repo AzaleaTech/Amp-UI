770b239eb114c512eb480a9361a82f5ee7a703f4
Chen zewei <zewei@azalea-tech.com>
2021-06-11 17:16:44 +0800

    WXCOM-18 Cropper组件

--- demo/pages/category/index.js
+++ demo/pages/category/index.js
@@ -7,11 +7,19 @@ Page({
       { label: 'Form & FormItem 表单及表单项', url: '../form/index' },
       { label: 'Verification 验证码按钮', url: '../verification/index' },
       { label: 'Collapse 可折叠列表', url: '../collapse/index' },
-      { label: 'Modal 遮罩层（含Toast,Dialog）', url: '../modal/index' },
+      { label: 'Modal 遮罩层（包括Dialog和Toast）', url: '../modal/index' },
+      { label: 'Cropper 图片剪裁', url: '../cropper/index' },
     ],
+    //用于图片剪裁
+    imgs: '/demo/assets/images/test-avatar.jpg'
   },
 
   handleClickFormItem(e) {
+    if(e.currentTarget.dataset.label ==  'Cropper 图片剪裁') {
+      wx.navigateTo({
+        url: e.currentTarget.dataset.url + `?imgs=${this.data.imgs}`,
+      });
+    }
     wx.navigateTo({ url: e.currentTarget.dataset.url });
   },
 });
