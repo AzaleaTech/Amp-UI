737932488635cc1f4ab47f3538cfcdf89fb8bfc5
caizhiquan <zhiquan@azalea-tech.com>
2021-07-07 16:39:01 +0800

    WXCOM-34 UploadImage组件优化

--- demo/pages/upload/index.js
+++ demo/pages/upload/index.js
@@ -15,71 +15,57 @@ Page({
     isCropper2: true,
   },
 
-  handleChooseImage1(e){
-    if(this.data.isCropper1){
-      this.data.image1.push(e.detail.value);
-    }
-    else {
-      this.data.image1.push(...e.detail.value);
-    }
+  handleChange1(e) {
     this.setData({
-      image1: this.data.image1,
+      image1: e.detail.imgList,
     });
   },
 
-  handleRemoveImage1(e){
-    this.data.image1.splice(e.detail, 1);
-    this.setData({ image1: this.data.image1 });
-  },
-
-  handleChooseImage2(e){
-    // 注意，若定义了isCropper2字段，则必须有下面的if判断
-    if(this.data.isCropper2){
-      this.data.image2.push(e.detail.value);
+  handleChange2(e) {
+    if (e.detail.status === 'success') {
+      this.setData({
+        image2: e.detail.imgList,
+      });
+    } else {
+      this.setData({
+        toastData: {
+          content: e.detail.msg,
+          type: 'error',
+        },
+      });
     }
-    else {
-      this.data.image2.push(...e.detail.value);
-    }
-    this.setData({
-      image2: this.data.image2,
-    });
   },
 
-  handleRemoveImage2(e){
-    this.data.image2.splice(e.detail, 1);
-    this.setData({ image2: this.data.image2 });
-  },
-
-  handleChooseVideo(e){
+  handleChooseVideo(e) {
     this.setData({
       videos: this.data.videos.concat(e.detail.value),
-    })
+    });
   },
 
-  handleRemoveVideo(e){
+  handleRemoveVideo(e) {
     this.data.videos.splice(e.detail, 1);
     this.setData({ videos: this.data.videos });
   },
 
-  handleChooseVideo1(e){
+  handleChooseVideo1(e) {
     this.setData({
       videos1: this.data.videos1.concat(e.detail.value),
-    })
+    });
   },
 
-  handleRemoveVideo1(e){
+  handleRemoveVideo1(e) {
     this.data.videos1.splice(e.detail, 1);
     this.setData({ videos1: this.data.videos1 });
   },
 
-  handleChooseVideo2(e){
+  handleChooseVideo2(e) {
     this.setData({
       videos2: this.data.videos2.concat(e.detail.value),
-    })
+    });
   },
 
-  handleRemoveVideo2(e){
+  handleRemoveVideo2(e) {
     this.data.videos2.splice(e.detail, 1);
     this.setData({ videos2: this.data.videos2 });
-  }
-})
+  },
+});
