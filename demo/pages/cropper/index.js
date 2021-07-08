12cbb7468184ea4248b564f07e54cec0568c3b4d
caizhiquan <zhiquan@azalea-tech.com>
2021-07-08 14:41:20 +0800

    WXCOM-27 Cropper组件修改DEMO第一次进入图片显示问题

--- demo/pages/cropper/index.js
+++ demo/pages/cropper/index.js
@@ -1,10 +1,15 @@
 Page({
   data: {
+    isCropper: false,
     image: '',
   },
 
   onLoad(e) {
     this.setData({ image: e.image });
+  },
+
+  handleClick() {
+    this.setData({ isCropper: true });
   },
 
   handleSave(e) {},
