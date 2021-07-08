cc42262d7f18c72b3aedbdb6a0363303957814bc
caizhiquan <zhiquan@azalea-tech.com>
2021-07-08 14:48:13 +0800

    WXCOM-36 Tabber组件顶部吸附实现更改

--- demo/pages/tabbar/ceiling/index.js
+++ demo/pages/tabbar/ceiling/index.js
@@ -1,21 +1,12 @@
 Page({
   data: {
     currentTab: 0,
-    isCeiling: false,
-    tabbarHeight: 41.6,
+    isCeiling: true,
     staticTabs: ['测试标签'],
     card: {
       title: '测试Tab页',
       contents: [{ label: '测试项', content: '测试Tab页' }],
       imgUrl: '/demo/assets/images/test-avatar.jpg',
     },
-  },
-
-  onPageScroll(res) {
-    if (res.scrollTop >= this.data.tabbarHeight && !this.data.isCeiling) {
-      this.setData({ isCeiling: true });
-    } else if (res.scrollTop < this.data.tabbarHeight && this.data.isCeiling) {
-      this.setData({ isCeiling: false });
-    }
   },
 });
