4eed17c0910025304e3e21ff1bb59666ee1ca0e8
Saxon <saxon@SaxondeMacBook-Pro.local>
2021-10-27 11:06:58 +0800

    WXCOM-55 tabs组件优化

--- demo/pages/tabs/ceiling/index.js
+++ demo/pages/tabs/ceiling/index.js
@@ -1,11 +1,11 @@
 Page({
   data: {
     currentTab: 0,
-    isCeiling: true,
+    ceiling: true,
     staticTabs: ['测试标签'],
     card: {
       title: '测试Tab页',
-      contents: [{ label: '测试项', content: '测试Tab页' }],              
+      contents: [{ label: '测试项', content: '测试Tab页' }],
       imgUrl: '/demo/assets/images/test-avatar.jpg',
     },
   },
