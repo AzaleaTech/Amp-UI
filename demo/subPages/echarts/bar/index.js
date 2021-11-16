f4697c57b64a20572e2f1ef3e34112bcf7f5f285
Saxon <saxon@SaxondeMacBook-Pro.local>
2021-11-16 17:31:51 +0800

    WXCOM-79 新增echarts 示例

---
+++ demo/subPages/echarts/bar/index.js
@@ -0,0 +1,42 @@
+import * as echarts from '../../components/Echarts/echarts';
+
+function initChart(canvas, width, height, dpr) {
+  const chart = echarts.init(canvas, null, {
+    width: width,
+    height: height,
+    devicePixelRatio: dpr, // new
+  });
+  canvas.setChart(chart);
+
+  const option = {
+    xAxis: {
+      type: 'category',
+      data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+    },
+    yAxis: {
+      type: 'value',
+    },
+    series: [
+      {
+        data: [120, 200, 150, 80, 70, 110, 130],
+        type: 'bar',
+        showBackground: true,
+        backgroundStyle: {
+          color: 'rgba(180, 180, 180, 0.2)',
+        },
+      },
+    ],
+  };
+
+  chart.setOption(option);
+
+  return chart;
+}
+
+Page({
+  data: {
+    ec: {
+      onInit: initChart,
+    },
+  },
+});
---
+++ demo/subPages/echarts/index/index.js
@@ -0,0 +1,29 @@
+Page({
+  data: {
+    lists: [
+      {
+        id: 'line',
+        name: '折线图',
+      },
+      {
+        id: 'bar',
+        name: '柱状图',
+      },
+      {
+        id: 'pie',
+        name: '饼图',
+      },
+      {
+        id: 'map',
+        name: '地图',
+      },
+    ],
+  },
+
+  handleTap(e) {
+    const { id } = e.target.dataset;
+    wx.navigateTo({
+      url: `../${id}/index`,
+    });
+  },
+});
