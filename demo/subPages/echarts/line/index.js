a36dea20339ca58429dd7970e2192924e4dabc6a
Saxonion <pengyuan@azalea-tech.com>
2021-11-16 17:31:51 +0800

    WXCOM-79 新增echarts 示例

---
+++ demo/subPages/echarts/line/index.js
@@ -0,0 +1,38 @@
+import * as echarts from '../../components/Echarts/echarts';
+
+function initChart(canvas, width, height, dpr) {
+  const chart = echarts.init(canvas, null, {
+    width: width,
+    height: height,
+    devicePixelRatio: dpr, // new
+  });
+  canvas.setChart(chart);
+
+  const option = {
+    xAxis: {
+      type: 'category',
+      data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+    },
+    yAxis: {
+      type: 'value',
+    },
+    series: [
+      {
+        data: [150, 230, 224, 218, 135, 147, 260],
+        type: 'line',
+      },
+    ],
+  };
+
+  chart.setOption(option);
+
+  return chart;
+}
+
+Page({
+  data: {
+    ec: {
+      onInit: initChart,
+    },
+  },
+});
