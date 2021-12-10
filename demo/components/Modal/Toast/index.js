bcf91db560ed2bb37d8937b4d5b9ff2441fc2462
Saxonion <pengyuan@azalea-tech.com>
2021-12-10 19:04:55 +0800

    WXCOM-100 全局图标替换

--- demo/components/Modal/Toast/index.js
+++ demo/components/Modal/Toast/index.js
@@ -16,13 +16,21 @@ Component({
 
   data: {
     show: false,
+    iconName: '',
+    iconColor: '',
+    iconTypes: {
+      success: ['succeed_fill', '#19BE6B'],
+      warning: ['about_fill', '#FFB500'],
+      error: ['error_fill', '#ED4114'],
+    },
   },
 
   observers: {
     // 弹框默认1.5s关闭
     toastData(val) {
       if (val) {
-        this.setData({ show: true });
+        const iconStatus = this.data.iconTypes[val.type || 'success'];
+        this.setData({ show: true, iconName: iconStatus[0], iconColor: iconStatus[1] });
         setTimeout(() => {
           this.setData({ show: false });
         }, val.duration || 1500);
