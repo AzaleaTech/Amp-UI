8c738d2b64bda44843162b8aca22b0461d3ac84a
Saxonion <pengyuan@azalea-tech.com>
2021-12-17 18:19:07 +0800

    WXCOM-101 record和player组件图标替换

--- demo/pages/audio/index.js
+++ demo/pages/audio/index.js
@@ -1,7 +1,7 @@
 Page({
   data: {
     audioUrl:
-      'http://ws.stream.qqmusic.qq.com/M500001VfvsJ21xFqb.mp3?guid=ffffffff82def4af4b12b3cd9337d5e7&uin=346897220&vkey=6292F51E1E384E06DCBDC9AB7C49FD713D632D313AC4858BACB8DDD29067D3C601481D36E62053BF8DFEAF74C0A5CCFADD6471160CAF3E6A&fromtag=46',
+      'http://m10.music.126.net/20211216122721/18dafe963bb02e9336fca0bae6af47db/yyaac/obj/wonDkMOGw6XDiTHCmMOi/1638726471/fe53/e830/8ea5/fd356e868caf782f89e92e739ebb862b.m4a',
   },
 
   // record组件方法
