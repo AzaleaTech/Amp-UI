e6845f8fac3d31d9f7dd7cac257085e117ead480
Saxon <saxon@SaxondeMacBook-Pro.local>
2021-12-31 11:02:51 +0800

    WXCOM-106 全局优化
    - 移除测试头像图片
    - 代码格式化

--- demo/pages/tabs/index/index.js
+++ demo/pages/tabs/index/index.js
@@ -7,7 +7,7 @@ Page({
     overflowTabs: ['标签1', '标签2', '标签3', '标签4', '标签5', '标签6', '标签7'],
     card: {
       title: '标题',
-      imgUrl: '/demo/assets/images/test-avatar.jpg',
+      imgUrl: 'https://res.wx.qq.com/wxdoc/dist/assets/img/0.4cb08bb4.jpg',
     },
   },
 
