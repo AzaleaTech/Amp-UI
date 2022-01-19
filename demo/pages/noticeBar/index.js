659177c569bee3ec30d742d32be23ccf1605666d
zhangwai <1519214533@qq.com>
2022-01-19 18:19:36 +0800

    WXCOM-112 新增 NoticeBar 通告栏组件

---
+++ demo/pages/noticeBar/index.js
@@ -0,0 +1,9 @@
+Page({
+  data: {
+    leftIcon: 'trumpet_fill',
+    text: '大家好，我是xxx，今天来到我的家里做客须知:1、不要乱碰家里的花瓶。2、不能随意上二楼。3、不要随意在家里乱扔垃圾。'
+  },
+  handleClick(e){
+    console.log(e.detail);
+  }
+});
