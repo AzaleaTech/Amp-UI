de67d60c2883cb19c898628c7bb308b5299d687e
Saxonion <765734929@qq.com>
2022-11-29 17:56:39 +0800

    WXCOM-164 UI优化

--- packages/amp-ui-demo/src/components/Tabs/index.js
+++ packages/amp-ui-demo/src/components/Tabs/index.js
@@ -36,5 +36,31 @@ Component({
       }
       this.triggerEvent('change', { value: current });
     },
+
+    handleScroll(e) {
+      // 默认遮罩宽度 px
+      const MARGIN = 32;
+      // 页面宽度
+      const WINDOW_HEIGHT = 375;
+      const { scrollLeft, scrollWidth } = e.detail;
+      if (scrollLeft > 0) {
+        this.setData({
+          showPrevModal: true,
+        });
+      } else {
+        this.setData({
+          showPrevModal: false,
+        });
+      }
+      if (scrollWidth - WINDOW_HEIGHT - scrollLeft > MARGIN) {
+        this.setData({
+          showLastModal: true,
+        });
+      } else {
+        this.setData({
+          showLastModal: false,
+        });
+      }
+    },
   },
 });
